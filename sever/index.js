919d9314fd36083693c3a2867c11b6deed1609c8
buithanhdat1002 <buivanthanhdat10022001@gmail.com>
2023-03-17 21:11:43 +0700

    commit ngay 17/3

--- sever/index.js
+++ sever/index.js
@@ -21,4 +21,4 @@ mongoose.connect(process.env.MONGO_URL, {
 const server = app.listen(process.env.PORT, () =>{
     console.log('Server Started on port ${process.env.PORT}');
 });
-ConvolverNode  
+ConvolverNode  await
